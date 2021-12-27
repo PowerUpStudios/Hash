3dea30ddd020f5380aac82b37ec7bdb20f7a9cd8
Power Up Studio <76599999+PowerUpStudios@users.noreply.github.com>
2021-12-27 15:27:11 +0100

    Update tokens.java

--- tokens.java
+++ tokens.java
@@ -6,3 +6,13 @@ SKIP :
 | "\r"
 | <"\"" (~["\n","\r"])* ("\n"|"\r"|"\r\n")>
 }
+TOKEN : /*
+KEYWORDS */
+{
+      < REQUERE: "require" >
+|     < IF: "if" >
+|     < WHILE: "while" >
+|     < DO: "do" >
+|     < STOP: "stop" >
+|     < DEF : "def" >
+}
